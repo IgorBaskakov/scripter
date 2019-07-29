5297c9b5da83ad8879abca491ec26c1456f0f017
Igor Baskakov <igor.baskak@gmail.com>
2019-07-30 00:20:46 +0300

    add getting body from response, add counter for result of requests

--- main.go
+++ main.go
@@ -3,14 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	const inlineWord = "Go"
 	in := bufio.NewScanner(os.Stdin)
 
 	for in.Scan() {
 		txt := in.Text()
-		fmt.Println(txt)
+
+		body, err := get(txt)
+		if err != nil {
+			log.Fatalf("cann't get content from %s", txt)
+		}
+		cnt := strings.Count(body, inlineWord)
+		fmt.Printf("Count for %s: %d\n", txt, cnt)
 	}
+}
+
+func get(uri string) (string, error) {
+	resp, err := http.Get(uri)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
 }
