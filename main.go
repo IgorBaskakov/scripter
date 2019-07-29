798813910af42f1f2b9d29261ba9a303abffc0d3
Igor Baskakov <igor.baskak@gmail.com>
2019-07-30 00:40:22 +0300

    add concurency for handlers, add rate limitter

--- main.go
+++ main.go
@@ -8,22 +8,44 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 func main() {
 	const inlineWord = "Go"
 	in := bufio.NewScanner(os.Stdin)
+	total, ratelimit := 0, 5
+	quotaCh := make(chan struct{}, ratelimit)
+	wg := &sync.WaitGroup{}
+	i := 0
 
 	for in.Scan() {
 		txt := in.Text()
 
-		body, err := get(txt)
-		if err != nil {
-			log.Fatalf("cann't get content from %s", txt)
-		}
-		cnt := strings.Count(body, inlineWord)
-		fmt.Printf("Count for %s: %d\n", txt, cnt)
+		quotaCh <- struct{}{}
+		datach := make(chan string)
+		wg.Add(1)
+		i++
+		go func(i int) {
+			fmt.Printf("start %d task\n", i)
+			body, err := get(txt)
+			if err != nil {
+				log.Fatalf("cann't get content from %s", txt)
+			}
+			datach <- body
+		}(i)
+
+		go func(i int) {
+			fmt.Printf("start count for %d task\n", i)
+			cnt := strings.Count(<-datach, inlineWord)
+			total += cnt
+			fmt.Printf("Count for %s: %d\n", txt, cnt)
+			<-quotaCh
+			wg.Done()
+		}(i)
 	}
+	wg.Wait()
+	fmt.Printf("Total: %d\n", total)
 }
 
 func get(uri string) (string, error) {
